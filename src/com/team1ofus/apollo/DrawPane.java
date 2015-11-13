42d37211e08e444f108d3918e91ea1458481d91a
Ares513 <evanfitzking@gmail.com>
2015-11-13 13:55:03 -0500

    Added classes to begin drawing data onto the screen.
    - Next steps include actually making CellRenderer
    - Need image data but for now will use solid colors
    - *hums peacefully, powerhousing code*

--- src/com/team1ofus/apollo/DrawPane.java
+++ src/com/team1ofus/apollo/DrawPane.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class DrawPane extends JPanel {
+	//THIS METHOD RENDERS CELLS
 	public void paintComponent(Graphics g) {
 		for(int i=0; i<100; i++) {
 			for(int j=0; j<100; j++) {
---
+++ src/com/team1ofus/apollo/TILE_TYPE.java
@@ -0,0 +1,6 @@
+package com.team1ofus.apollo;
+
+public enum TILE_TYPE {
+	WALL, //cannot path through a wall
+	 HALLWAY //can path through a hallway from any direction to any direction
+ }
