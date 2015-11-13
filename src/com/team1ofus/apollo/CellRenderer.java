2c0ec84ce5f4f743d1b59fae0f06bd9af93d88e9
Ares513 <evanfitzking@gmail.com>
2015-11-13 15:55:24 -0500

    Now supports drawing tiles based on data 	- dummy tile data in use 	- need tile picking/data connection

--- src/com/team1ofus/apollo/CellRenderer.java
+++ src/com/team1ofus/apollo/CellRenderer.java
@@ -1,5 +1,34 @@
 package com.team1ofus.apollo;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class CellRenderer {
+	DataTile[][] tiles;
+	int tileWidth;
+	int tileHeight;
+	public CellRenderer(DataTile[][] tiles) {
+		this.tiles = tiles;
+		tileWidth = 32;
+		tileHeight = 32;
+	}
+	public void renderTiles(Graphics g) {
+		for(int i=0; i<tiles[0].length; i++) {
+			for(int j=0; j<tiles[1].length; j++) {
+				switch(tiles[i][j].getType()) {
+				case WALL:
+					g.setColor(Color.gray);
+					
+					break;
+				case HALLWAY:
+					g.setColor(Color.WHITE);
+					break;
+				}
+				g.fillRect(i*tileWidth, j*tileHeight, tileWidth, tileHeight);
+				g.setColor(Color.black);
+				g.drawRect(i*tileWidth, j*tileHeight, tileWidth, tileHeight);
+			}
+		}
+	}
 	
 }
--- src/com/team1ofus/apollo/DrawPane.java
+++ src/com/team1ofus/apollo/DrawPane.java
@@ -5,13 +5,22 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class DrawPane extends JPanel {
-	//THIS METHOD RENDERS CELLS
-	public void paintComponent(Graphics g) {
-		for(int i=0; i<100; i++) {
-			for(int j=0; j<100; j++) {
-				g.drawRect(i*20, j*20, 20, 20);
+	CellRenderer render;
+	public DrawPane() {
+		//test data
+		DataTile[][] dummyData;
+		dummyData = new DataTile[50][50];
+		
+		for(int i=0; i<50; i++) {
+			for(int j=0; j<50; j++) {
+				dummyData[i][j] = new DataTile(TILE_TYPE.WALL);
 			}
 		}
+		dummyData[1][2] = new DataTile(TILE_TYPE.HALLWAY);
+		render = new CellRenderer(dummyData);
+	}
+	public void paintComponent(Graphics g) {
+		render.renderTiles(g);
 	}
 	
 }
