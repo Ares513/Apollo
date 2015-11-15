d9e864b5ac472e30dba45e4078fbde63ef839658
Bryan Toribio <batoribio@wpi.edu>
2015-11-15 13:22:18 -0500

    Created enum TILE_TYPE that holds the different tiles we will be using

--- src/com/team1ofus/apollo/TILE_TYPE.java
+++ src/com/team1ofus/apollo/TILE_TYPE.java
@@ -2,5 +2,14 @@ package com.team1ofus.apollo;
 
 public enum TILE_TYPE {
 	WALL, //cannot path through a wall
-	 HALLWAY //can path through a hallway from any direction to any direction
+	HALLWAY, //can path through a hallway from any direction to any direction
+	DOOR,    //
+	PEDESTRIAN_WALKWAY,
+	GRASS,
+	CONGESTED,
+	STAIRS,
+	OUTDOOR_STAIRS,
+	STEEP, //can path through. Represents hills(inclines in general)
+	BATHROOM	
+	
  }
