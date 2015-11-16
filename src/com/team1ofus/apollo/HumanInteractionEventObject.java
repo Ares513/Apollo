7359dc35e191d0ece570389d4e4b844cf1d0d4ae
Ares513 <evanfitzking@gmail.com>
2015-11-16 16:41:10 -0500

    Added a ton of classes that make up the framework for our event model.
    Lots of questions about how we're doing this, so I'll leave an
    explanation here.
    Basically, Bootstrapper will implement two interfaces, one for UI and
    one for data. The EventObject is stored wherever the events are held. It
    is told to 'fire' an event and send a message to all classes that are
    'listening'. Since it's an interface, each of them gets to pick their
    response individually, thus creating a event model.

--- src/com/team1ofus/apollo/HumanInteractionEventObject.java
+++ src/com/team1ofus/apollo/HumanInteractionEventObject.java
@@ -2,4 +2,12 @@ package com.team1ofus.apollo;
 
 public class HumanInteractionEventObject {
 
+	
+	public synchronized void addMouseClickListener() {
+		
+	}
+	public synchronized void removeMouseClickListener() {
+		
+	}
 }
+
